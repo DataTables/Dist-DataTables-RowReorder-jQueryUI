06159b0ac275b202df419db4f78d1a2aa98136f9
Allan Jardine <allan@datatables.net>
2022-10-10 19:50:38 +0000

    Add support for mjs files (ES Modules)

---
+++ js/rowReorder.jqueryui.mjs
@@ -0,0 +1,13 @@
+
+/*! jQuery UI styling wrapper for RowReorder
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-ju';
+import DataTable from 'datatables.net-rowreorder';
+
+
+
+
+export default DataTable;
