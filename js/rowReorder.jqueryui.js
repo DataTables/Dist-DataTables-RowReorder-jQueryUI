98526c8cc2f114b3b5712f3712d1cf8dec93c13b
dtbuild <dtbuild@rigil>
2026-09-25 13:31:29 +0000

    025085178766203f9389d4ba9e639703f439c0dd Release 2.1.1
    Sync to source repo @025085178766203f9389d4ba9e639703f439c0dd

--- js/rowReorder.jqueryui.js
+++ js/rowReorder.jqueryui.js
@@ -1,4 +1,4 @@
-/*! RowReorder jQuery UI styling 2.1.0 for DataTables
+/*! RowReorder jQuery UI styling 2.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
--- js/rowReorder.jqueryui.min.mjs
+++ js/rowReorder.jqueryui.min.mjs
@@ -1,4 +1,4 @@
-/*! RowReorder jQuery UI styling 2.1.0 for DataTables
+/*! RowReorder jQuery UI styling 2.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-jqui";import RowReorder from"datatables.net-rowreorder";export default DataTable;
